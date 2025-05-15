ed35d7d37291389ee1451dfed21f2c99c02a50b0
Martin-gldr <martin.glandier@gmail.com>
2025-05-15 13:19:40 +0200

    maj main

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import { createRoot } from 'react-dom/client'
 import './style/index.scss'
 import HomePage from './Composants/HomePage/HomePage.jsx';
-import APropos from './Composants/APropos.jsx';
-import FicheLogement from "./Composants/FicheLogement.jsx";
+import APropos from './Composants/APropos/APropos.jsx';
+import FicheLogement from "./Composants/FicheLogement/FicheLogement.jsx";
 import {createBrowserRouter,RouterProvider} from "react-router";
 
 let router = createBrowserRouter([
   {path: "/",element: <HomePage />, errorElement: <div>Erreur</div>},
   {path:"/APropos", element: <APropos />},
-  {path:"/FicheLogement", element: <FicheLogement />}
+  {path:"/FicheLogement/:datasId", element: <FicheLogement />}
 ]);
 
 
